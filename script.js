6cf3af633068ee8e591003879728ff860d6bfc93
mdtanvirgit <officialtanvir@hotmail.com>
2023-04-09 10:42:26 +0600

    my website services site is working

---
+++ script.js
@@ -0,0 +1,45 @@
+const product = document.getElementById('product');
+
+fetch(`http://localhost:5000/foods`)
+    .then(res => res.json())
+    .then(data => {
+        displayFoods(data)
+    })
+
+
+const displayFoods = (data) => {
+    data.forEach(element => {
+        console.log(element)
+        const div = document.createElement('div');
+
+        product.appendChild(div);
+
+        div.innerHTML = `
+            <div class=' text-center border w-96 flex flex-col justify-center items-center p-4 space-y-2'>
+                <img class="w-80" src=${element.image} />
+                <h1 class="font-bold">${element.product_name}</h1>
+            </div>
+        `
+
+    });
+}
+
+
+
+document.getElementById('search_btn').addEventListener('click', () => {
+    const search = document.getElementById('search').value
+
+
+    fetch(`http://localhost:5000/foods?search=${search}`)
+        .then(res => res.json())
+        .then(data => {
+            displayFoods(data)
+
+        })
+
+    // fetch(`http://localhost:5000/foods?search=${search}`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //     })
+})
