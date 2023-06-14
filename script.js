14b6e6115b855d34b13a7347a5d0ab77ac0bf619
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-06-14 11:46:13 +0600

    some bug fix this website

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const product = document.getElementById('product');
 
-fetch(`https://product-backend-mdtanvirgit.vercel.app/foods`)
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`)
     .then(res => res.json())
     .then(data => {
-        displayFoods(data)
+        displayFoods(data.meals)
     })
 
 
 const displayFoods = (data) => {
+
     data.forEach(element => {
         console.log(element)
         const div = document.createElement('div');
@@ -15,31 +16,41 @@ const displayFoods = (data) => {
         product.appendChild(div);
 
         div.innerHTML = `
-            <div class=' text-center border w-96 flex flex-col justify-center items-center p-4 space-y-2'>
-                <img class="w-80" src=${element.image} />
-                <h1 class="font-bold">${element.product_name}</h1>
+            <div class=' text-center border lg:w-96 flex flex-col justify-center items-center p-4 space-y-2'>
+                <img class="lg:w-80" src=${element.strMealThumb} />
+                <h1 class="font-bold">${element.strMeal
+            }</h1>
             </div>
         `
 
     });
+
 }
 
 
-
-document.getElementById('search_btn').addEventListener('click', () => {
-    const search = document.getElementById('search').value
-
-
-    fetch(`https://product-backend-mdtanvirgit.vercel.app/foods?search=${search}`)
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userSearch = e.target.search.value
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userSearch}`)
         .then(res => res.json())
         .then(data => {
-            displayFoods(data)
-
+            console.log(data);
+            product.innerHTML = ''
+            displayFoods(data.meals)
         })
 
-    // fetch(`http://localhost:5000/foods?search=${search}`)
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data);
-    //     })
+
 })
+
+// document.getElementById('search_btn').addEventListener('click', (e) => {
+//     e.preventDefault()
+//     const search = document.getElementById('search').value
+
+
+//
+//     // fetch(`http://localhost:5000/foods?search=${search}`)
+//     //     .then(res => res.json())
+//     //     .then(data => {
+//     //         console.log(data);
+//     //     })
+// })
